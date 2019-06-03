b60a3b337ef94f351a1cb92d88b8a44db27d71c6
Baditsa Valeriy <valerabaditsa@gmail.com>
2019-06-03 17:59:36 +0300

    Added parsing Price and Image

--- ParsingStore_App/ParserManager/Parser.cs
+++ ParsingStore_App/ParserManager/Parser.cs
@@ -6,6 +6,8 @@ using System.Net;
 using System.Text;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
 
 namespace ParsingStore_App.ParserManager
 {
@@ -26,10 +28,81 @@ namespace ParsingStore_App.ParserManager
                 ParsedProduct resultProduct = new ParsedProduct { Title = title };
                 parsedProductList.Add(resultProduct);
             }
-            
+
+
+            // parse product Price
+            HtmlNodeCollection prices = document.DocumentNode.SelectNodes
+               (".//div[@class='content-product__price']//div[@class='product-price']//" +
+               "span[@class='wc-price wc-price-on-sale wc-price-variable']"); //  //span[@class='wc-price-sale']//span[@class='wc-price-number'] 
+            int j = 0;
+
+            int n = 0;
+            foreach (HtmlNode price in prices)
+            {
+                if (price != null)
+                {
+                    string decodedString = price.InnerText.Replace("&nbsp; &#8372;", " грн."); // ₴InnerText = "\n            от 2 619&nbsp; &#8372;        "
+                    decodedString = decodedString.Replace("  ", string.Empty);
+                    decodedString = decodedString.Replace("\n", string.Empty);
+                    //productList.ElementAt(j++).Price = (decodedString);
+
+
+                    parsedProductList.ElementAt(n++).Price = decodedString;
+
+                }
+            }
+
+
+            // parce product image .//li//div[@class='content-product__thumb']//a//img
+            HtmlNodeCollection imgs = document.DocumentNode.SelectNodes
+              (".//div[@class='content-product__thumb']//a//img"); //content - product__thumb
+            int k = 0;
+            foreach (HtmlNode imgUrl in imgs)
+            {
+                string urlImage = imgUrl.Attributes["data-srcset"].Value;// ("DeEntitizeValue", null);// DeEntitizeValue = "https://badminton.ua/wp-content/themes/badminton/public/img/logo/logo-icon.png"
+                string[] arr_Url = urlImage.Split(' ');
+                urlImage = arr_Url[0];
+                if (urlImage != null)
+                {
+                    Image imageCurProd = DownloadImageByUrl(urlImage);
+                    byte[] bytesArrForImage = imageToByteArray(imageCurProd);
+                    //productList.ElementAt(k++).ImageBytes = bytesArrForImage.Select(a=>(byte?)a).ToArray();
+                    parsedProductList.ElementAt(k++).ImageBytes = bytesArrForImage;
+                }
+
+
+            }
+
+
             return parsedProductList;
         }
-        
+
+        public static Image DownloadImageByUrl(string imageUrl)
+        {
+            Image image = null;
+            try
+            {
+                HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.
+                    Create(imageUrl);
+                webRequest.AllowWriteStreamBuffering = true;
+                webRequest.Timeout = 30000;
+
+                System.Net.WebResponse webResponse = webRequest.GetResponse();
+
+                System.IO.Stream stream = webResponse.GetResponseStream();
+
+                image = System.Drawing.Image.FromStream(stream);
+
+                webResponse.Close();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            return image;
+        }
+
         static private byte[] imageToByteArray(System.Drawing.Image imageIn)
         {
             MemoryStream ms = new MemoryStream();
